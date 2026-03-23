088a37ba894000613e704c4724e2aa732e903b78
Minh Thien <ngthien0911@gmail.com>
2026-03-23 21:29:56 +0700

    UPDATE

---
+++ C5-LTWEB/mainpage.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+if (isset($_SESSION["user"])) {
+    echo "<h2>TRANG CHÍNH</h2>";
+    echo "Người dùng đã đăng nhập với tên <b>" 
+        . $_SESSION["user"] . "</b> và Email là <b>" 
+        . $_SESSION["email"] . "</b>";
+
+    echo "<br><a href='logout.php'>Đăng xuất</a>";
+} else {
+    echo "Bạn chưa đăng nhập!";
+}
+?>
